e7136d0f202d682cb4499bdce7a1eaed8a62e30c
Bertrand Chenal <bertrand@adimian.com>
2016-11-28 16:10:04 +0100

    Add encoding test

---
+++ tests/encoding_test.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+from tanker import View
+
+from .base_test import session
+
+
+def test_read_write(session):
+    japan = '日本'.decode('utf-8')
+    team_view = View('country', ['name'])
+    team_view.write([(japan,)])
+
+    row = team_view.read(filter_by={'name': japan}).next()
+    assert row[0] == japan
