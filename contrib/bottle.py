fd3f04e92c35990c5bf60764a9a660f52b75f254
Bertrand Chenal <bertrand@adimian.com>
2019-03-15 08:43:57 +0100

    Add contrib

---
+++ contrib/bottle.py
@@ -0,0 +1,28 @@
+# Plugin to integrate Tanker with Bottle. It creates a new tanker
+# connection on each request (tanker re-use pooled pg connections)
+
+# Usage:
+# from bottle import install
+# install(BottlePlugin())
+
+from functools import wraps
+from tanker import connect
+
+
+class TankerPlugin():
+    '''
+    Plugin class to add tanker support to a bottle app
+    '''
+
+    name = 'TankerPlugin'
+    api = 2
+
+    def __init__(self, cfg):
+        self.cfg = cfg
+
+    def apply(self, callback, route):
+        @wraps(callback)
+        def wrapper(*args, **kwargs):
+            with connect(self.cfg):
+                return callback(*args, **kwargs)
+        return wrapper
